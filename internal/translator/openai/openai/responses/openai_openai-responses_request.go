arsydoni4326-alt/CLIProxyAPI

fix：openai 2 kimi error   Continuous function_call 连续的function_call 转换 tool_calls

internal/translator/openai/openai/responses/openai_openai-responses_request.go
@@ -57,7 +57,24 @@ func ConvertOpenAIResponsesRequestToOpenAIChatCompletions(modelName string, inpu
 
 	// Convert input array to messages
 	if input := root.Get("input"); input.Exists() && input.IsArray() {
+		inputItems := input.Array()
+		outputCallIDs := make(map[string]struct{})
+		for _, item := range inputItems {
+			if item.Get("type").String() != "function_call_output" {
+				continue
+			}
+			callID := strings.TrimSpace(item.Get("call_id").String())
+			if callID == "" {
+				continue
+			}
+			outputCallIDs[callID] = struct{}{}
+		}
+
 		pendingToolCalls := make([]interface{}, 0)
+		pendingToolCallIDs := make([]string, 0)
+		awaitingToolOutputs := make(map[string]struct{})
+		deferredMessages := make([][]byte, 0)
+
 		flushPendingToolCalls := func() {
 			if len(pendingToolCalls) == 0 {
 				return
@@ -65,10 +82,40 @@ func ConvertOpenAIResponsesRequestToOpenAIChatCompletions(modelName string, inpu
 			assistantMessage := []byte(`{"role":"assistant","tool_calls":[]}`)
 			assistantMessage, _ = sjson.SetBytes(assistantMessage, "tool_calls", pendingToolCalls)
 			out, _ = sjson.SetRawBytes(out, "messages.-1", assistantMessage)
+			for _, id := range pendingToolCallIDs {
+				if strings.TrimSpace(id) == "" {
+					continue
+				}
+				awaitingToolOutputs[id] = struct{}{}
+			}
 			pendingToolCalls = pendingToolCalls[:0]
+			pendingToolCallIDs = pendingToolCallIDs[:0]
+		}
+		flushDeferredMessages := func() {
+			for _, message := range deferredMessages {
+				out, _ = sjson.SetRawBytes(out, "messages.-1", message)
+			}
+			deferredMessages = deferredMessages[:0]
+		}
+		hasAwaitingToolOutput := func() bool {
+			for id := range awaitingToolOutputs {
+				if _, ok := outputCallIDs[id]; ok {
+					return true
+				}
+			}
+			return false
+		}
+		appendRegularMessage := func(message []byte) {
+			// Keep tool-call adjacency strict for providers that require
+			// assistant(tool_calls) -> tool(tool_call_id) with no message in between.
+			if hasAwaitingToolOutput() {
+				deferredMessages = append(deferredMessages, message)
+				return
+			}
+			out, _ = sjson.SetRawBytes(out, "messages.-1", message)
 		}
 
-		input.ForEach(func(_, item gjson.Result) bool {
+		for _, item := range inputItems {
 			itemType := item.Get("type").String()
 			if itemType == "" && item.Get("role").String() != "" {
 				itemType = "message"
@@ -123,7 +170,7 @@ func ConvertOpenAIResponsesRequestToOpenAIChatCompletions(modelName string, inpu
 					message, _ = sjson.SetBytes(message, "content", content.String())
 				}
 
-				out, _ = sjson.SetRawBytes(out, "messages.-1", message)
+				appendRegularMessage(message)
 
 			case "function_call":
 				// Buffer consecutive function calls and emit them as one assistant message.
@@ -141,13 +188,18 @@ func ConvertOpenAIResponsesRequestToOpenAIChatCompletions(modelName string, inpu
 					toolCall, _ = sjson.SetBytes(toolCall, "function.arguments", arguments.String())
 				}
 				pendingToolCalls = append(pendingToolCalls, gjson.ParseBytes(toolCall).Value())
+				if callID := strings.TrimSpace(item.Get("call_id").String()); callID != "" {
+					pendingToolCallIDs = append(pendingToolCallIDs, callID)
+				}
 
 			case "function_call_output":
 				// Handle function call output conversion to tool message
 				toolMessage := []byte(`{"role":"tool","tool_call_id":"","content":""}`)
+				callID := ""
 
 				if callId := item.Get("call_id"); callId.Exists() {
-					toolMessage, _ = sjson.SetBytes(toolMessage, "tool_call_id", callId.String())
+					callID = strings.TrimSpace(callId.String())
+					toolMessage, _ = sjson.SetBytes(toolMessage, "tool_call_id", callID)
 				}
 
 				if output := item.Get("output"); output.Exists() {
@@ -155,11 +207,17 @@ func ConvertOpenAIResponsesRequestToOpenAIChatCompletions(modelName string, inpu
 				}
 
 				out, _ = sjson.SetRawBytes(out, "messages.-1", toolMessage)
+				if callID != "" {
+					delete(awaitingToolOutputs, callID)
+				}
+				if len(awaitingToolOutputs) == 0 && len(deferredMessages) > 0 {
+					flushDeferredMessages()
+				}
 			}
 
-			return true
-		})
+		}
 		flushPendingToolCalls()
+		flushDeferredMessages()
 	} else if input.Type == gjson.String {
 		msg := []byte(`{}`)
 		msg, _ = sjson.SetBytes(msg, "role", "user")

zuohuadong/CliRelay

(cherry picked from commit 17a1f53c47c2b6d846cd4cc928428c2824cb0ce5)

internal/translator/openai/openai/responses/openai_openai-responses_request.go
@@ -57,7 +57,24 @@ func ConvertOpenAIResponsesRequestToOpenAIChatCompletions(modelName string, inpu
 
 	// Convert input array to messages
 	if input := root.Get("input"); input.Exists() && input.IsArray() {
+		inputItems := input.Array()
+		outputCallIDs := make(map[string]struct{})
+		for _, item := range inputItems {
+			if item.Get("type").String() != "function_call_output" {
+				continue
+			}
+			callID := strings.TrimSpace(item.Get("call_id").String())
+			if callID == "" {
+				continue
+			}
+			outputCallIDs[callID] = struct{}{}
+		}
+
 		pendingToolCalls := make([]interface{}, 0)
+		pendingToolCallIDs := make([]string, 0)
+		awaitingToolOutputs := make(map[string]struct{})
+		deferredMessages := make([]string, 0)
+
 		flushPendingToolCalls := func() {
 			if len(pendingToolCalls) == 0 {
 				return
@@ -65,10 +82,40 @@ func ConvertOpenAIResponsesRequestToOpenAIChatCompletions(modelName string, inpu
 			assistantMessage := `{"role":"assistant","tool_calls":[]}`
 			assistantMessage, _ = sjson.Set(assistantMessage, "tool_calls", pendingToolCalls)
 			out, _ = sjson.SetRaw(out, "messages.-1", assistantMessage)
+			for _, id := range pendingToolCallIDs {
+				if strings.TrimSpace(id) == "" {
+					continue
+				}
+				awaitingToolOutputs[id] = struct{}{}
+			}
 			pendingToolCalls = pendingToolCalls[:0]
+			pendingToolCallIDs = pendingToolCallIDs[:0]
+		}
+		flushDeferredMessages := func() {
+			for _, message := range deferredMessages {
+				out, _ = sjson.SetRaw(out, "messages.-1", message)
+			}
+			deferredMessages = deferredMessages[:0]
+		}
+		hasAwaitingToolOutput := func() bool {
+			for id := range awaitingToolOutputs {
+				if _, ok := outputCallIDs[id]; ok {
+					return true
+				}
+			}
+			return false
+		}
+		appendRegularMessage := func(message string) {
+			// Keep tool-call adjacency strict for providers that require
+			// assistant(tool_calls) -> tool(tool_call_id) with no message in between.
+			if hasAwaitingToolOutput() {
+				deferredMessages = append(deferredMessages, message)
+				return
+			}
+			out, _ = sjson.SetRawBytes(out, "messages.-1", message)
 		}
 
-		input.ForEach(func(_, item gjson.Result) bool {
+		for _, item := range inputItems {
 			itemType := item.Get("type").String()
 			if itemType == "" && item.Get("role").String() != "" {
 				itemType = "message"
@@ -123,7 +170,7 @@ func ConvertOpenAIResponsesRequestToOpenAIChatCompletions(modelName string, inpu
 					message, _ = sjson.Set(message, "content", content.String())
 				}
 
-				out, _ = sjson.SetRaw(out, "messages.-1", message)
+				appendRegularMessage(message)
 
 			case "function_call":
 				// Buffer consecutive function calls and emit them as one assistant message.
@@ -141,13 +188,18 @@ func ConvertOpenAIResponsesRequestToOpenAIChatCompletions(modelName string, inpu
 					toolCall, _ = sjson.Set(toolCall, "function.arguments", arguments.String())
 				}
 				pendingToolCalls = append(pendingToolCalls, gjson.Parse(toolCall).Value())
+				if callID := strings.TrimSpace(item.Get("call_id").String()); callID != "" {
+					pendingToolCallIDs = append(pendingToolCallIDs, callID)
+				}
 
 			case "function_call_output":
 				// Handle function call output conversion to tool message
 				toolMessage := `{"role":"tool","tool_call_id":"","content":""}`
+				callID := ""
 
 				if callId := item.Get("call_id"); callId.Exists() {
-					toolMessage, _ = sjson.Set(toolMessage, "tool_call_id", callId.String())
+					callID = strings.TrimSpace(callId.String())
+					toolMessage, _ = sjson.Set(toolMessage, "tool_call_id", callID)
 				}
 
 				if output := item.Get("output"); output.Exists() {
@@ -155,11 +207,17 @@ func ConvertOpenAIResponsesRequestToOpenAIChatCompletions(modelName string, inpu
 				}
 
 				out, _ = sjson.SetRaw(out, "messages.-1", toolMessage)
+				if callID != "" {
+					delete(awaitingToolOutputs, callID)
+				}
+				if len(awaitingToolOutputs) == 0 && len(deferredMessages) > 0 {
+					flushDeferredMessages()
+				}
 			}
 
-			return true
-		})
+		}
 		flushPendingToolCalls()
+		flushDeferredMessages()
 	} else if input.Type == gjson.String {
 		msg := "{}"
 		msg, _ = sjson.Set(msg, "role", "user")